fc8a1438e072ddba60bcd1880c298d89ebd00f27
javacpcx <54021436+javacpcx@users.noreply.github.com>
2024-07-06 15:24:17 +0800

    Add files via upload
    fix error

--- Lab4-newServerPlcModbusUI.py
+++ Lab4-newServerPlcModbusUI.py
@@ -2,8 +2,10 @@ from flask import Flask, render_template, redirect, url_for, request
 from pyModbusTCP.server import ModbusServer, DataBank
 import threading
 import time
+
 app = Flask(__name__)
 server = ModbusServer(host="0.0.0.0", port=502, no_block=True)
+
 def modbus_server():
     try:
         server.start()
@@ -17,6 +19,7 @@ def modbus_server():
         server.stop()
     finally:
         server.stop()
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if server.is_run:
@@ -25,26 +28,30 @@ def index():
         status = "Stopped"
     registers = DataBank.get_words(0, 10) or [0] * 10
     return render_template('newIndex.html', status=status, registers=registers, enumerate=enumerate)
+
 @app.route('/update_registers', methods=['POST'])
 def update_registers():
     for i in range(10):
         new_value = int(request.form[f'register_{i}'])
         DataBank.set_words(i, [new_value])
-    return redirect(url_for('newIndex'))
+    return redirect(url_for('index'))
+
 @app.route('/start')
 def start_server():
     if not server.is_run:
         global modbus_thread
         modbus_thread = threading.Thread(target=modbus_server)
         modbus_thread.start()
-    return redirect(url_for('newIndex'))
+    return redirect(url_for('index'))
+
 @app.route('/stop')
 def stop_server():
     if server.is_run:
         server.stop()
         global modbus_thread
         modbus_thread.join()
-    return redirect(url_for('newIndex'))
+    return redirect(url_for('index'))
+
 if __name__ == '__main__':
     modbus_thread = threading.Thread(target=modbus_server)
     modbus_thread.start()
